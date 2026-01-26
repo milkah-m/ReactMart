fd0e0068ca74ea3be57bd82c76e1f7233201f35c
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-01-26 14:17:09 +0300

    Made a new folder containing the React.jsx and added component mapping inside the route container

---
+++ src/router/Router.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home.jsx';
+import Shop from '../pages/Shop.jsx';
+import Cart from '../pages/Cart.jsx';
+import About from '../pages/About.jsx';
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/shop' element={<Shop />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/about' element={<About />} />
+        </Routes>
+    );
+};
+
+export default Router;
