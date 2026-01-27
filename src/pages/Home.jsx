9f3b79ea93bc1e5a88c5eaa07f6146c271dfc8e7
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-01-27 19:36:18 +0300

    Reworked the home.jsx skeleton and added various class names for future styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
+
+import React from 'react';
+
 export default function Home() {
     return (
         <div className="home">
-            <section>
-                <h1>Fresh Groceries delivered to your Door</h1>
-                <p>Shop fresh fruits, vegetables and daily essentials with ease</p>
-                <button>Shop Now</button>
-            </section>
+            <header className='hero-banner'>
+                <nav className='navbar'>
+                    <div className='logo'>Grocery Logo</div>
+                    <ul className='nav-links'>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Products</a></li>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Contact</a></li>
+                    </ul>
+                </nav>
+                <div className='hero-content'>
+                    <h1>Fresh Groceries Delivered To Your Door</h1>
+                    <p>Shop fresh fruits, vegetables and daily essentials with ease</p>
+                    <div className='hero-btn'>
+                        <button className='shop-btn'>Shop Now</button>
+                    </div>
+                </div>
+            </header>
         </div>
     );
 }
